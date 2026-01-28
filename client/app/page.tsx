0016e800a9add10b31ccd30956bc045a063d8b3a
VD-Phuc-2k5 <duyvo1954@gmail.com>
2026-01-29 02:56:31 +0700

    feat(todo): add TodoList component to display todos

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Button } from "@/components/Button/Button";
 import Image from "next/image";
 import { Counter } from "@/components/Counter/Counter";
+import { TodoList } from "@/components/TodoList/TodoList";
 
 export default function HomePage() {
   return (
@@ -12,6 +13,7 @@ export default function HomePage() {
       mt-2 cursor-pointer rounded-full active:bg-black active:text-white transition-all'
       />
       <Counter />
+      <TodoList />
     </React.Fragment>
   );
 }
